850d196356c6c073f2baf933ea10287941c45d82
Gabriela <gabifleitas15@gmail.com>
2025-02-18 13:50:05 +0300

    Update sprint_3_manipulación_de_datos_(data_wrangling) (1).py

--- sprint_3_manipulación_de_datos_(data_wrangling) (1).py
+++ sprint_3_manipulación_de_datos_(data_wrangling) (1).py
@@ -1,3 +1,10 @@
+#Este es uno de los proyectos de práctica que he realizado en el bootcamp de Tripleten. 
+#Primero he resuelto la hipótesis que ellos formularon y luego hice algunos descubrimientos por mi cuenta. 
+
+#This is one of the practice projects I did at the Tripleten bootcamp. 
+#I first solved the hypothesis they formulated and then made some discoveries on my own. 
+
+
 # -*- coding: utf-8 -*-
 """Sprint 3: Manipulación de datos (Data Wrangling)
 
